f1aae2fc5f06d83772923a0f1a0eb5ce49dc6f49
JuanFUbeira <franciscoubeira@gmail.com>
2024-01-03 18:47:59 -0300

    more files, engine setup

---
+++ actividad_handlebars/src/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import handlebars from 'express-handlebars'
+import __dirname from './utils.js'
+
+const app = express()
+const port = 8080
+
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+// Engine setup
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    defaultLayout: 'main'
+}))
+
+// Set views
+app.set('view engine', 'hbs')
+app.set('views', `${__dirname}/views`)
+
+// Public folder
+app.use(express.static(`${__dirname}/public`))
+
+// Routes
+app.use('/', viewRouter)
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
