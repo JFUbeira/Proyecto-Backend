04e124cc04ff53a72731231c643f40d05c7bd7be
JuanFUbeira <franciscoubeira@gmail.com>
2024-01-16 17:07:54 -0300

    adjusted format of GET method response

--- segunda_pre_entrega/src/routes/views.router.js
+++ segunda_pre_entrega/src/routes/views.router.js
@@ -7,17 +7,35 @@ const router = Router()
 const productManager = new ProductManager()
 
 router.get('/api/products', async (req, res) => {
-    const { page, limit } = req.query
+    const { page, limit } = req.query;
 
-    const products = await productModel.paginate(
-        {},
-        {
+    try {
+        const products = await productModel.paginate({}, {
             page: page || 1,
             limit: limit || 10,
-        }
-    )
+        });
 
-    res.json(products)
+        const response = {
+            status: 'success',
+            payload: products.docs,
+            totalPages: products.totalPages,
+            prevPage: products.hasPrevPage ? products.prevPage : null,
+            nextPage: products.hasNextPage ? products.nextPage : null,
+            page: products.page,
+            hasPrevPage: products.hasPrevPage,
+            hasNextPage: products.hasNextPage,
+            prevLink: products.hasPrevPage ? `/api/products?page=${products.prevPage}&limit=${limit || 10}` : null,
+            nextLink: products.hasNextPage ? `/api/products?page=${products.nextPage}&limit=${limit || 10}` : null,
+        };
+
+        res.json(response);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            status: 'error',
+            message: 'Internal Server Error',
+        });
+    }
 })
 
 router.get('/api/products/:pid', (req, res) => {
