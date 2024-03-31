74c3d085f768fb122da457eff19a1e0ed3a851b1
JuanFUbeira <franciscoubeira@gmail.com>
2024-03-31 20:28:28 -0300

    created controllers and code for the routers

--- servidor_por_capas/src/routers/carts.router.js
+++ servidor_por_capas/src/routers/carts.router.js
@@ -1,107 +1,14 @@
 import { Router } from "express"
-import CartManager from "../dao/mongoManagers/MDBcartManager.js"
+import { createCart, updateCart, updateProductQuantity, getPopulatedCart, addProductToCart, deleteProductFromCart, deleteCart } from "../controllers/carts.controller.js"
 
 const router = Router()
-const cartManager = new CartManager()
 
-router.post("/", (req, res) => {
-    cartManager.createCart()
-        .then(cart => {
-            res.json(cart)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-})
-
-router.put("/:cid", async (req, res) => {
-    try {
-        const cid = req.params.cid
-        const updatedCart = req.body
-        await cartManager.updateCart(cid, updatedCart)
-        res.json({ status: 'success', message: 'Cart updated successfully' })
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ status: 'error', message: 'Internal Server Error' })
-    }
-})
-
-router.put("/:cid/product/:pid", async (req, res) => {
-    const cid = req.params.cid
-    const pid = req.params.pid
-    const quantity = req.body.quantity
-    try {
-        const validation = await cartManager.checkIfProductExists(pid)
-        if (validation) {
-            await cartManager.updateProductQuantity(cid, pid, quantity)
-            res.json({ status: 'success', message: 'Product quantity updated successfully' })
-        } else {
-            res.json({ status: 'error', message: 'Product not found' })
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ status: 'error', message: 'Internal Server Error' })
-    }
-})
-
-router.get("/:cid", async (req, res) => {
-    try {
-        const cid = req.params.cid
-        const populatedCart = await cartManager.getPopulatedCart(cid)
-        res.json(populatedCart)
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ status: 'error', message: 'Internal Server Error' })
-    }
-})
-
-
-router.post("/:cid/product/:pid", async (req, res) => {
-    const cid = req.params.cid
-    const pid = req.params.pid
-
-    try {
-        const validation = await cartManager.checkIfProductExists(pid)
-
-        if (validation) {
-            await cartManager.addProductToCart(cid, pid)
-            res.json({ status: 'success', message: 'Product added successfully' })
-        } else {
-            res.json({ status: 'error', message: 'Product not found' })
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ status: 'error', message: 'Internal Server Error' })
-    }
-})
-
-
-router.delete("/:cid/product/:pid", async (req, res) => {
-    const cid = req.params.cid
-    const pid = req.params.pid
-
-    try {
-        const validation = await cartManager.checkIfProductExists(pid)
-        if (validation) {
-            await cartManager.deleteProductFromCart(cid, pid)
-            res.json({ status: 'success', message: 'Product deleted successfully' })
-        } else {
-            res.json({ status: 'error', message: 'Product not found' })
-        }
-    } catch (error) {
-        console.log(error)
-    }
-})
-
-router.delete("/:cid", async (req, res) => {
-    const cid = req.params.cid
-
-    try {
-        await cartManager.deleteCart(cid)
-        res.json({ status: 'success', message: 'Cart deleted successfully' })
-    } catch (error) {
-        console.log(error)
-    }
-})
+router.post("/", createCart)
+router.put("/:cid", updateCart)
+router.put("/:cid/product/:pid", updateProductQuantity)
+router.get("/:cid", getPopulatedCart)
+router.post("/:cid/product/:pid", addProductToCart)
+router.delete("/:cid/product/:pid", deleteProductFromCart)
+router.delete("/:cid", deleteCart)
 
 export default router
