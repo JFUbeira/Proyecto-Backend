d0db7f7fabdb54770fe6f84a9f8b291fb1dfc821
JuanFUbeira <franciscoubeira@gmail.com>
2024-04-03 23:51:39 -0300

    desafio mocking first commit

---
+++ desafio_mocking/src/app.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import session from 'express-session'
+import config from './config/config.js'
+import MongoStore from 'connect-mongo'
+import MongoSingleton from './config/mongodb-singleton.js'
+import cors from 'cors'
+import passport from 'passport'
+import initializePassport from './config/passport.config.js'
+
+// import Routers
+// import usersRouter from './routers/users.router.js'
+import productsRouter from './routers/products.router.js'
+import cartsRouter from './routers/carts.router.js'
+import sessionsRouter from './routers/sessions.router.js'
+import emailRouter from './routers/email.router.js'
+// import smsRouter from './routers/sms.router.js'
+
+const app = express()
+
+// JSON settings and middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+
+// session settings
+app.use(
+    session({
+        store: MongoStore.create({
+            mongoUrl: config.mongoURL,
+        }),
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: false },
+    })
+);
+
+// passport settings
+initializePassport()
+app.use(passport.initialize())
+app.use(passport.session())
+
+// routes declaration 
+// app.use('/api/users', usersRouter)
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
+app.use('/api/sessions', sessionsRouter)
+app.use('/api/email', emailRouter)
+// app.use('/api/sms', smsRouter)
+
+const server_port = config.port
+app.listen(server_port, () => {
+    console.log(`Server running on port ${server_port}`)
+})
+
+// Mongo instance
+const mongoInstance = async () => {
+    try {
+        await MongoSingleton.getInstance()
+        console.log('MongoDB connected')
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+mongoInstance()
---
+++ desafio_mocking/src/services/dao/mongoManagers/MDBusers.dao.js
@@ -0,0 +1,76 @@
+import userModel from '../../models/user.model.js'
+class UserManager {
+    async getUsers() {
+        try {
+            const users = await userModel.find();
+            return users;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getUserById(id) {
+        try {
+            const user = await userModel.findById(id);
+            if (!user) {
+                console.log('Error: User not found');
+            } else {
+                return user;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async createUser(user) {
+        try {
+            const { first_name, last_name, email, age, password } = user;
+
+            if (!first_name || !last_name || !email || !age || !password) {
+                console.log('Error: All fields are required');
+            } else if (await userModel.findOne({ email })) {
+                console.log('Error: User already exists');
+            } else {
+                const newUser = new userModel({
+                    first_name,
+                    last_name,
+                    email,
+                    age,
+                    password,
+                    role: 'user'
+                });
+                await newUser.save();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async updateUser(uid, updatedUser) {
+        try {
+            const user = await userModel.findById(uid);
+
+            if (!user) {
+                console.log('Error: User not found');
+            } else {
+                Object.assign(user, updatedUser); // Update fields
+                await user.save();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async deleteUser(id) {
+        try {
+            const result = await userModel.findByIdAndDelete(id);
+            if (!result) {
+                console.log('Error: User not found');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export default UserManager;
